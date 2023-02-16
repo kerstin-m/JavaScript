31685ce412943df3de75c3acf23510912819b3b7
KerstinM <missyayadivine@gmail.com>
2023-02-16 10:47:41 +0100

    feat, task-o5

---
+++ task-05/script.js
@@ -0,0 +1,79 @@
+/*const size = 25;
+let result;
+
+if (size > 20) {
+  result = "Greater than 20";
+} else {
+  result = "Lower than 10";
+}
+
+console.log(result);
+// Returns Greater than 10 but should return Greater than 20
+*/
+
+// TODO: Implement the oddEven function
+/*
+const number = 4;
+let result;
+
+if (number % 2 == 0) {
+  result = "Even";
+} else {
+  result = "odd";
+}
+console.log(result);
+// result should be even
+*/
+
+/*
+const number = 3;
+let result;
+if (number % 2 == 0) {
+  result = "Even";
+} else {
+  result = "odd";
+}
+console.log(result);
+// result should be odd
+
+
+const number = -1;
+let result;
+
+if (number % 2 == 0) {
+  result = "Even";
+} else {
+  result = "odd";
+}
+console.log(result);
+// result should be odd
+*/
+const number = 10;
+let result;
+if (number % 2 == 0) {
+  result = "Even";
+} else {
+  result = "odd";
+}
+console.log(result);
+// result should be even
+
+// TODO: Implement the oldYoung function
+
+const age = 27;
+let oldYoung;
+if (age < 50) {
+  oldYoung = "young person";
+}
+
+console.log(oldYoung);
+// result should be young person
+
+console.log(oldYoung(6));
+// result should be children
+
+console.log(oldYoung(-1));
+// result should be invalid parameter
+
+console.log(oldYoung(86));
+// result should be elder person
